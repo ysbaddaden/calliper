acf44238f9e0d37fe94e0de30dcef221782f6f1b
Julien Portalier <julien@portalier.com>
2014-02-06 20:33:45 +0100

    Saucelabs travis configuration

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,6 +11,8 @@ Calliper.setup do |config|
     config.driver = :remote
     config.remote_url = "http://#{ENV['SAUCE_USERNAME']}:#{ENV['SAUCE_ACCESS_KEY']}@ondemand.saucelabs.com/wd/hub"
     config.capabilities = {
+      'name' => 'Calliper',
+      'build' => ENV['TRAVIS_BUILD_NUMBER'],
       'tunnel-identifier' => ENV['TRAVIS_JOB_NUMBER'],
       'browserName' => ENV['BROWSER'] || 'firefox',
     }
